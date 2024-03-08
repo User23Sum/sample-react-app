460c02f42426c8e4629c5dc5e0ec88e9d5a4a263
Summit Limbu <123644354+User23Sum@users.noreply.github.com>
2024-03-08 11:20:13 +0000

    Change of URL

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://automatic-space-xylophone-667pwvvpgp6c6xw-3000.app.github.dev/";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
